ec0b643f88ba4eb4d278b590ed473a907e937e58
Alejandro de Ugarriza <aledeum.saf@gmail.com>
2020-11-25 17:17:22 -0300

    Agregada clasificacion de victimas

--- Alumnos/Alejandro_de_Ugarriza/PseudoToCode.py
+++ Alumnos/Alejandro_de_Ugarriza/PseudoToCode.py
@@ -443,6 +443,7 @@ class Camera:
         self.height = self.camera.getHeight()
         self.width = self.camera.getWidth()
         self.tileRanges = tileRanges
+        self.classifyThresh = 50
 
     # Gets an image from the raw camera data
     def getImg(self):
@@ -507,6 +508,49 @@ class Camera:
         #print("Shape: " + str(img.shape))
         #print("Status: " + str(status))
         return status
+
+    def classifyVictim(self, img):
+        img = cv.resize(img, (100, 100))
+        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        thresh1 = cv.threshold(gray, 100, 255, cv.THRESH_BINARY_INV)[1]
+        conts, h = cv.findContours(thresh1, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        x, y, w, h = cv.boundingRect(conts[0])
+        letter = thresh1[y:y + h, x:x + w]
+        letter = cv.resize(letter, (100, 100), interpolation=cv.INTER_AREA)
+        #letterColor = cv.cvtColor(letter, cv.COLOR_GRAY2BGR)
+        areaWidth = 20
+        areaHeight = 30
+        areas = {
+            "top": ((0, areaHeight),(50 - areaWidth // 2, 50 + areaWidth // 2)),
+            "middle": ((50 - areaHeight // 2, 50 + areaHeight // 2), (50 - areaWidth // 2, 50 + areaWidth // 2)),
+            "bottom": ((100 - areaHeight, 100), (50 - areaWidth // 2, 50 + areaWidth // 2 ))
+            }
+        images = {
+            "top": letter[areas["top"][0][0]:areas["top"][0][1], areas["top"][1][0]:areas["top"][1][1]],
+            "middle": letter[areas["middle"][0][0]:areas["middle"][0][1], areas["middle"][1][0]:areas["middle"][1][1]],
+            "bottom": letter[areas["bottom"][0][0]:areas["bottom"][0][1], areas["bottom"][1][0]:areas["bottom"][1][1]]
+            }
+        #cv.rectangle(letterColor,(areas["top"][1][0], areas["top"][0][0]), (areas["top"][1][1], areas["top"][0][1]), (0, 255, 0), 1)
+        #cv.rectangle(letterColor, (areas["middle"][1][0], areas["middle"][0][0]), (areas["middle"][1][1], areas["middle"][0][1]), (0, 0, 255), 1)
+        #cv.rectangle(letterColor,(areas["bottom"][1][0], areas["bottom"][0][0]), (areas["bottom"][1][1], areas["bottom"][0][1]), (225, 0, 255), 1)
+        counts = {}
+        for key in images.keys():
+            count = 0
+            for row in images[key]:
+                for pixel in row:
+                    if pixel == 255:
+                        count += 1
+            counts[key] = count > self.classifyThresh
+        letters = {
+            "H":{'top': False, 'middle': True, 'bottom': False},
+            "S":{'top': True, 'middle': True, 'bottom': True},
+            "U":{'top': False, 'middle': False, 'bottom': True}
+            }
+        for letterKey in letters.keys():
+            if counts == letters[letterKey]:
+                finalLetter = letterKey
+                break
+        return finalLetter
                 
 # Sends messages
 class Emitter:
@@ -817,6 +861,12 @@ class AbstractionLayer:
                 victimInRange = True
                 break
         return victimInRange
+    
+    def getVictimLetter(self, camera):
+        for pos, img in zip(r.cameras[camera]["poses"], r.cameras[camera]["images"]):
+            if self.cameras[camera]["camera"].getVictimRange(pos, img) == 0:
+                return self.cameras[camera]["camera"].classifyVictim(img)
+
         
     def seqMove(self,ratio1, ratio2):
         if self.seqMg.check():
@@ -1106,7 +1156,9 @@ while r.update():
         r.startSequence()
         r.seqMove(0,0)
         if r.seqDelaySec(3):
-            r.sendMessage("N")
+            letter = r.getVictimLetter("centre")
+            print("Detected letter: " + str(letter))
+            r.sendMessage(letter)
             r.grid.setPosition(r.globalPos, "collectedVictim")
             r.calculatePath()
             r.changeState("main")
