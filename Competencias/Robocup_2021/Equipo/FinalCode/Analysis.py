458b8df80ab79290f0daf4a031f3bf873a0017e7
Alejandro de Ugarriza <aledeum.saf@gmail.com>
2021-05-05 21:27:26 -0300

    Agregados comentarios a Anilysis.py

--- Competencias/Robocup_2021/Equipo/FinalCode/Analysis.py
+++ Competencias/Robocup_2021/Equipo/FinalCode/Analysis.py
@@ -33,17 +33,18 @@ class VortexNode:
         self.dimensions = [0.01, 0.01, 0.06] # Dimensions of the vortex
         self.occupied = False # If there is a vortex. Can be True or false.
 
+# A virtual representation of the competition map
 class Grid:
     
     def __init__(self, chunk, initialSize):
-        self.startingSize = initialSize
-        self.size = [2, 2]
-        self.offsets = [0, 0]
-        self.grid = [[]]
-        self.chunk = chunk
+        self.startingSize = initialSize # The initial size of the grid, cant be 0 and has to be divisible by the size of the chunk
+        self.size = [2, 2] # The actual size of the grid
+        self.offsets = [0, 0] # Offsets of the grid to allow negative indexes
+        self.grid = [[]] # The grid containing the data
+        self.chunk = chunk # A chunk of nodes constituting the grid
         self.__constructGrid()
         
-        
+    # Given a string indicating direction returns an array directing to that direction
     def directionToNumber(self, direction):
         if direction == "center" or direction == "centre":
             n = [0, 0]
@@ -65,6 +66,7 @@ class Grid:
             n = [-1, -1]
         return n
 
+    # Constructs the grid
     def __constructGrid(self):
         self.grid = self.chunk.copy()
         for _ in range((self.startingSize[0] // 2) - 1):
@@ -76,7 +78,8 @@ class Grid:
         self.offsets[1] = self.startingSize[1] // 2
 
         self.size = self.startingSize
-        
+    
+    # Adds a row at the end of the grid
     def addRowAtEnd(self):
         row = self.chunk.copy()
         if self.size[0] > 1:
@@ -84,7 +87,8 @@ class Grid:
                 row = np.hstack((row.copy(), self.chunk.copy()))
             self.grid = np.vstack((self.grid.copy(), row.copy()))
             self.size[1] += 2
-            
+    
+    # Adds a row at the start of the grid
     def addRowAtStart(self):
         row = self.chunk.copy()
         if self.size[0] > 1:
@@ -94,6 +98,7 @@ class Grid:
             self.size[1] += 2
             self.offsets[1] += 2
 
+    # Adds a column at the end of the grid
     def addColumnAtEnd(self):
         column = self.chunk.copy()
         if self.size[1] > 1:
@@ -102,6 +107,7 @@ class Grid:
             self.grid = np.hstack((self.grid.copy(), column.copy()))
             self.size[0] += 2
 
+    # Adds a column at the start of the grid
     def addColumnAtStart(self):
         column = self.chunk.copy()
         if self.size[1] > 1:
@@ -111,17 +117,19 @@ class Grid:
             self.size[0] += 2
             self.offsets[0] += 2
     
-
+    # returns the node in the position in the grid taking in to account offsets
     def getRawNode(self, position):
         x = position[0] + self.offsets[0]
         y = position[1] + self.offsets[1]
         return self.grid[x][y]
     
+    # Sets a value in the position in the grid taking in to account offsets
     def setRawNode(self, position, value):
         x = position[0] + self.offsets[0]
         y = position[1] + self.offsets[1]
         self.grid[x][y] = value
 
+    # Returns a node given the position of a tile and directions to indicate walls and vertices
     def getNode(self, position, side=[0,0]):
         if isinstance(side, str):
             x = position[0] * 2 + self.directionToNumber(side)[0]
@@ -131,6 +139,7 @@ class Grid:
             y = position[1] * 2 + side[1]
         return self.getRawNode((x, y))
     
+    # Sets a node given the position of a tile and directions to indicate walls and vertices
     def setNode(self, position, value, side=[0,0]):
         if isinstance(side, str):
             x = position[0] * 2 + self.directionToNumber(side)[0]
@@ -144,6 +153,7 @@ class Grid:
     def duplicate(self, node):
         self.grid[node[0], node[1]] +=  10
 
+# Finds the best path to follow
 class PathFinder:
 
     def __init__(self):
@@ -176,14 +186,15 @@ chunk1 = np.array([[0, 1],
 chunk2 = np.array([[VortexNode(), WallNode()],
                    [WallNode()  , TileNode()]])
 
-grid = Grid(chunk2, (10, 10))
+grid = Grid(chunk1, [10, 10])
 print("--------------")
 
+grid.addRowAtStart()
 print(grid.grid)
 print("Offsets: " + str(grid.offsets))
 
 
 print("--------------")
 
-print(grid.getNode((2, 2), side="up").isCurved)
+#print(grid.getNode((2, 2), side="up").isCurved)
 
