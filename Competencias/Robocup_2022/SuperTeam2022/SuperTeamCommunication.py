f899558c0c32c26ac9d99c8109b55a429514bc98
JoaquinRodriguez2006 <joaquienrodriguez760@gmail.com>
2022-07-16 18:56:04 -0300

    add superteam code

--- Competencias/Robocup_2022/SuperTeam2022/SuperTeamCommunication.py
+++ Competencias/Robocup_2022/SuperTeam2022/SuperTeamCommunication.py
@@ -1,4 +1,4 @@
-from controller import Robot, Emitter
+from controller import Robot, Emitter, Receiver
 import struct
 
 robot = Robot()
@@ -36,3 +36,24 @@ class communication():
         message = struct.pack(struct_fmt,data)
         self.team_emitter.send(message)
         return True
+    
+receiver = robot.getDevice("receiver") # Retrieve the receiver and emitter by device name
+emitter = robot.getDevice("emitter")
+receiver.enable(TimeStep)
+
+def recieve_and_decode_messages_from_team():
+    if receiver.getQueueLength() > 0:
+        message = receiver.getData()
+        receiver.nextPacket()
+        struct_fmt = "S"
+        message = struct.unpack(struct_fmt, message)
+        data = {"robot_id:" + (message[0])}
+        print("Data: ", data)
+        return data
+
+def send_messages_to_other_team():
+        struct_fmt = "T"
+        data = "Im done"
+        message = struct.pack(struct_fmt, *data)
+        emitter.send(message)
+        print("I've just sent the message! It says: ", message)
