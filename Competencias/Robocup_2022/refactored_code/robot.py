162f00b49d1c7f5ba7b86391006974623e7b8444
ale <aledeum.saf@gmail.com>
2023-03-13 11:50:02 -0300

    Implemented encapsulated low level movement into robot

--- Competencias/Robocup_2022/refactored_code/robot.py
+++ Competencias/Robocup_2022/refactored_code/robot.py
@@ -15,8 +15,9 @@ from devices.gyroscope import Gyroscope
 
 from devices.comunicator import Comunicator
 
-from low_level_movement.drive_base import RotationManager, Criteria
+from low_level_movement.drive_base import DriveBase, Criteria
 from data_structures.angle import Angle, Unit
+from data_structures.vectors import Position2D, Vector2D
 
 from flags import SHOW_DEBUG
 
@@ -24,8 +25,7 @@ from flags import SHOW_DEBUG
 # In charge of low level movement
 class RobotLayer:
     def __init__(self, time_step):
-        # Maximum wheel speed
-        self.max_wheel_speed = 6.28
+        
         # The timestep
         self.time_step = time_step
         
@@ -48,20 +48,21 @@ class RobotLayer:
         self.delay_first_time = True
         self.delay_start = self.robot.getTime()
 
+        # Position and rotation sensors
         self.auto_decide_rotation = True
         self.rotation_sensor = "gyro"
 
-        #Sensors
         self.gyroscope = Gyroscope(self.robot.getDevice("gyro"), 1, self.time_step)
         self.gps = Gps(self.robot.getDevice("gps"), self.time_step)
 
-
+        # LIDAR
         lidar_interval = 6
         self.lidar = Lidar(webots_device = self.robot.getDevice("lidar"), 
                            time_step = self.time_step * lidar_interval, 
                            step_counter = utilities.StepCounter(lidar_interval),
                            layers_used=(2,))
 
+        # Cameras
         camera_interval = 3
         self.center_camera = Camera(webots_device = self.robot.getDevice("camera1"),
                                     time_step = self.time_step * camera_interval,
@@ -76,13 +77,17 @@ class RobotLayer:
                                   step_counter = utilities.StepCounter(camera_interval),
                                   rotate180=True)
 
-        #Actuators
+        
         self.comunicator = Comunicator(self.robot.getDevice("emitter"), self.robot.getDevice("receiver"), self.time_step)
-        self.left_wheel = Wheel(self.robot.getDevice("wheel1 motor"), self.max_wheel_speed)
-        self.right_wheel = Wheel(self.robot.getDevice("wheel2 motor"), self.max_wheel_speed)
+        
+        # Low level movement
+        # Maximum wheel speed
+        max_wheel_speed = 6.28
+        self.drive_base = DriveBase(left_wheel = Wheel(self.robot.getDevice("wheel1 motor"), max_wheel_speed), 
+                                    right_wheel = Wheel(self.robot.getDevice("wheel2 motor"), max_wheel_speed),
+                                    max_wheel_velocity = max_wheel_speed)
 
-        self.rot_manager = RotationManager(self.left_wheel, self.right_wheel)
-
+        # Stuck counter
         self.stuck_counter = 0
 
     def delay_sec(self, delay):
@@ -98,45 +103,23 @@ class RobotLayer:
                 return True
         return False
 
-    # Moves the wheels at the specified ratio
+    # Wrappers for DriveBase
+    @property
+    def max_wheel_speed(self):
+        return self.drive_base.max_wheel_velocity
+
     def move_wheels(self, left_ratio, right_ratio):
-        self.left_wheel.move(left_ratio)
-        self.right_wheel.move(right_ratio)
+        self.drive_base.move_wheels(left_ratio, right_ratio)
 
     def rotate_to_angle(self, angle, direction=Criteria.CLOSEST):
-        self.rot_manager.rotate_to_angle(angle, direction)
-        return self.rot_manager.finished_rotating
+        return self.drive_base.rotate_to_angle(Angle(angle, Unit.DEGREES), direction)
 
     def move_to_coords(self, targetPos):
-        errorMargin = 0.01
-        descelerationStart = 0.5 * 0.12
-        diffX = targetPos[0] - self.position[0]
-        diffY = targetPos[1] - self.position[1]
-        # print("Target Pos: ", targetPos)
-        # print("Used global Pos: ", self.position)
-        # print("diff in pos: " + str(diffX) + " , " + str(diffY))
-        dist = utilities.getDistance((diffX, diffY))
-        if SHOW_DEBUG: print("Dist: "+ str(dist))
-        if errorMargin * -1 < dist < errorMargin:
-            # self.robot.move(0,0)
-            if SHOW_DEBUG: print("FinisehedMove")
-            return True
-        else:
-            
-            ang = utilities.getDegsFromCoords((diffX, diffY))
-            ang = utilities.normalizeDegs(ang)
-            # print("traget ang: " + str(ang))
-            ratio = min(utilities.mapVals(dist, 0, descelerationStart, 0.1, 1), 1)
-            ratio = max(ratio, 0.8)
-            if self.rotate_to_angle(Angle(ang, Unit.DEGREES)):
-                self.move_wheels(ratio, ratio)
-                # print("Moving")
-        return False
-    
+        return self.drive_base.move_to_position(Position2D(targetPos[0], targetPos[1]))
     
     # Wrappers for lidar
     @property
-    def point_is_close(self):
+    def point_is_close(self) -> bool:
         return self.lidar.is_point_close
 
     def get_point_cloud(self):
@@ -145,24 +128,20 @@ class RobotLayer:
     def get_out_of_bounds_point_cloud(self):
         return self.lidar.get_out_of_bounds_point_cloud()
     
-    # Wrappers for camera images
+    # Wrapper for cameras
     def get_camera_images(self):
         if self.center_camera.step_counter.check():
             return [self.right_camera.get_image(), 
                     self.center_camera.get_image(), 
                     self.left_camera.get_image()]
     
-    # Starts loop ans returns True if the simulation is running
+    # Loops and returns True if the simulation is running
     def do_loop(self):
         return self.robot.step(self.time_step) != -1
     
-    def get_wheel_direction(self):
-        if self.right_wheel.velocity + self.left_wheel.velocity == 0:
-            return 0
-        return (self.right_wheel.velocity + self.left_wheel.velocity) / 2
     
     def is_stuck_this_step(self):
-        return self.get_wheel_direction() > 0 and abs(utilities.getDistance(utilities.substractLists(self.position, self.prev_global_position))) < 0.00001
+        return self.drive_base.get_wheel_direction() > 0 and abs(utilities.getDistance(utilities.substractLists(self.position, self.prev_global_position))) < 0.00001
 
     def is_stuck(self):
         return self.stuck_counter > 50
@@ -185,7 +164,7 @@ class RobotLayer:
         # Decides wich sensor to use for roatation detection
         # if the robot is going srtaight i tuses the gps
         if self.auto_decide_rotation:
-            if self.gyroscope.getDiff() < 0.00001 and self.get_wheel_direction() >= 0:
+            if self.gyroscope.getDiff() < 0.00001 and self.drive_base.get_wheel_direction() >= 0:
                 self.rotation_sensor = "gps"
             # if it isn't going straight it uses the gyro
             else:
@@ -207,7 +186,8 @@ class RobotLayer:
                 self.rotation = val
             self.gyroscope.setDegrees(self.rotation)
 
-        self.rot_manager.current_angle = Angle(self.rotation, Unit.DEGREES)
+        self.drive_base.orientation = Angle(self.rotation, Unit.DEGREES)
+        self.drive_base.position = Position2D(self.position[0], self.position[1])
 
         # Lidar and camera update
         self.lidar.setRotationDegrees(self.rotation + 0)
