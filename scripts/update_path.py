cc75180937d02ad21a0ca0b2146912e792cf0c74
ale <aledeum.saf@gmail.com>
2023-03-17 10:49:05 -0300

    copied the script to change the improt path of the run file

---
+++ scripts/update_path.py
@@ -0,0 +1,24 @@
+#run after installing repo!!
+
+# Modifes "run.py" to add the actual directory to sys.path
+
+import os
+import pathlib
+
+script_dir = pathlib.Path(os.path.dirname(__file__))
+rel_path = pathlib.Path("src/run.py")
+
+abs_target_file_path = pathlib.Path(script_dir.parent, rel_path)
+
+file = ""
+
+with open(abs_target_file_path, "r") as code:
+    file += f"absuloute_dir = r'{abs_target_file_path.parent}'\n"
+    code.readline()
+    file += code.read()
+
+with open(abs_target_file_path, "w") as code:
+    code.write(file)
+    
+
+
