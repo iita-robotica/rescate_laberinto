implemented a new array with a gradient from all detected points. This is meant to indicate a preference for this point in navigation. Fixed a bug with the expanding grid: a matter of indices being switch, nothing too complex

src/data_structures/compound_pixel_grid.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2 as cv
 import copy
 from data_structures.vectors import Position2D
+from utilities import StepCounter
 
 class PointGrid:
     def __init__(self, initial_shape, pixel_per_cm, robot_radius_m):
@@ -22,7 +23,7 @@ class PointGrid:
                                ('fixture', np.unicode_, 1),
                                ])
         
-        self.to_boolean_threshold = 25
+        self.to_boolean_threshold = 10#25
         self.delete_threshold = 1
 
         self.data_grid = np.zeros(self.array_shape, self.dtype)
@@ -30,27 +31,34 @@ class PointGrid:
         self.detected_points_grid = np.zeros(self.array_shape, np.uint8)
         self.occupied_grid = np.zeros(self.array_shape, np.bool_)
         self.traversable_grid = np.zeros(self.array_shape, np.bool_)
+        self.navigation_preference_grid = np.zeros(self.array_shape, np.float32)
 
-        self.resolution = pixel_per_cm
-        self.robot_radius = int(robot_radius_m * 1000 // pixel_per_cm)
+        self.resolution = pixel_per_cm * 100
+        
+        self.robot_radius = int(robot_radius_m * self.resolution)
+        print("ROBOT RADIOUS:", self.robot_radius)
         self.robot_diameter = int(self.robot_radius * 2 + 1)
-        self.robot_circle_template = np.zeros((self.robot_diameter, self.robot_diameter), dtype=np.uint8)
-        self.robot_circle_template = cv.circle(self.robot_circle_template, (self.robot_radius, self.robot_radius), self.robot_radius, 255, -1)
 
-        self.robot_circle_template = self.robot_circle_template.astype(np.bool_)
+        self.robot_diameter_template = np.zeros((self.robot_diameter, self.robot_diameter), dtype=np.uint8)
+        self.robot_diameter_template = cv.circle(self.robot_diameter_template, (self.robot_radius, self.robot_radius), self.robot_radius, 255, -1)
+        self.robot_diameter_template = self.robot_diameter_template.astype(np.bool_)
+
+        self.preference_template = self.__generate_circle_gradient(self.robot_radius, self.robot_radius * 2)
 
     def coordinates_to_grid_index(self, coordinates):
         if isinstance(coordinates, np.ndarray):
-            coords = (coordinates * 1000 // self.resolution).astype(int)
+            coords = (coordinates * self.resolution).astype(int)
 
         return np.array([coords[1], coords[0]])
 
     def grid_index_to_coordinates(self, grid_index):
         if isinstance(grid_index, np.ndarray):
-            index = (grid_index.astype(float) * self.resolution) / 1000
+            index = (grid_index.astype(float) / self.resolution)
         
         return np.array([index[1], index[0]])
         
+    def clean_up(self):
+        self.detected_points_grid = self.detected_points_grid * (self.detected_points_grid > self.delete_threshold)
 
     def load_point_cloud(self, point_cloud, robot_position):
         for p in point_cloud:
@@ -69,11 +77,16 @@ class PointGrid:
                     self.detected_points_grid[position[0], position[1]] += 1
                 elif self.detected_points_grid[position[0], position[1]] >= self.to_boolean_threshold:
                     self.occupied_grid[position[0], position[1]] = True
+       
+        self.clean_up()
         
-        self.detected_points_grid = self.detected_points_grid * (self.detected_points_grid > self.delete_threshold)
-        
-        self.traversable_grid = cv.filter2D(self.occupied_grid.astype(np.uint8), -1, self.robot_circle_template.astype(np.uint8))
+        occupied_as_int = self.occupied_grid.astype(np.uint8)
+
+        self.traversable_grid = cv.filter2D(occupied_as_int, -1, self.robot_diameter_template.astype(np.uint8))
         self.traversable_grid = self.traversable_grid.astype(np.bool_)
+
+        self.navigation_preference_grid = cv.filter2D(occupied_as_int, -1, self.preference_template)
+
 
     def array_index_to_grid_index(self, array_index: np.ndarray):
         return array_index[0] - self.offsets[0], array_index[1] - self.offsets[1]
@@ -88,10 +101,11 @@ class PointGrid:
 
         if array_index[1] + 1 > self.array_shape[1]:
             self.add_end_column(array_index[1] - self.array_shape[1] + 1)
-        if array_index[1] < 0:
-            self.add_begining_row(array_index[1] * -1)
+
         if array_index[0] < 0:
-            self.add_begining_column(array_index[0] * -1)
+            self.add_begining_row(array_index[0] * -1)
+        if array_index[1] < 0:
+            self.add_begining_column(array_index[1] * -1)
     
     def add_end_row(self, size):
         self.array_shape = np.array([self.array_shape[0] + size, self.array_shape[1]])
@@ -100,16 +114,18 @@ class PointGrid:
         self.occupied_grid =    self.__add_end_row_to_grid(self.occupied_grid, size)
         self.traversable_grid = self.__add_end_row_to_grid(self.traversable_grid, size)
         self.detected_points_grid = self.__add_end_row_to_grid(self.detected_points_grid, size)
+        self.navigation_preference_grid = self.__add_end_row_to_grid(self.navigation_preference_grid, size)
         
     
     def add_begining_row(self, size):
-        self.offsets[1] += size
+        self.offsets[0] += size
         self.array_shape = np.array([self.array_shape[0] + size, self.array_shape[1]])
         
         self.data_grid =        self.__add_begining_row_to_grid(self.data_grid, size)
         self.occupied_grid =    self.__add_begining_row_to_grid(self.occupied_grid, size)
         self.traversable_grid = self.__add_begining_row_to_grid(self.traversable_grid, size)
         self.detected_points_grid = self.__add_begining_row_to_grid(self.detected_points_grid, size)
+        self.navigation_preference_grid = self.__add_begining_row_to_grid(self.navigation_preference_grid, size)
 
     def add_end_column(self, size):
         self.array_shape = np.array([self.array_shape[0], self.array_shape[1] + size])
@@ -118,15 +134,17 @@ class PointGrid:
         self.occupied_grid =    self.__add_end_column_to_grid(self.occupied_grid, size)
         self.traversable_grid = self.__add_end_column_to_grid(self.traversable_grid, size)
         self.detected_points_grid = self.__add_end_column_to_grid(self.detected_points_grid, size)
+        self.navigation_preference_grid = self.__add_end_column_to_grid(self.navigation_preference_grid, size)
 
     def add_begining_column(self, size):
-        self.offsets[0] += size
+        self.offsets[1] += size
         self.array_shape = np.array([self.array_shape[0], self.array_shape[1] + size])
 
         self.data_grid =        self.__add_begining_column_to_grid(self.data_grid, size)
         self.occupied_grid =    self.__add_begining_column_to_grid(self.occupied_grid, size)
         self.traversable_grid = self.__add_begining_column_to_grid(self.traversable_grid, size)
         self.detected_points_grid = self.__add_begining_column_to_grid(self.detected_points_grid, size)
+        self.navigation_preference_grid = self.__add_begining_column_to_grid(self.navigation_preference_grid, size)
 
     def __add_end_row_to_grid(self, grid, size):
         grid = np.vstack((grid, np.zeros((size, self.array_shape[1]), dtype=grid.dtype)))
@@ -143,6 +161,16 @@ class PointGrid:
     def __add_begining_column_to_grid(self, grid, size):
         grid = np.hstack((np.zeros((self.array_shape[0], size), dtype=grid.dtype), grid))
         return grid
+    
+    def __generate_circle_gradient(self, min_radius, max_radius):
+        min_radius = round(min_radius)
+        max_radius = round(max_radius)
+        template = np.zeros((max_radius * 2 + 1, max_radius * 2 + 1), dtype=np.float32)
+        for i in range(max_radius, min_radius, -1):
+            print("i:", i)
+            template = cv.circle(template, (max_radius, max_radius), i, max_radius ** 2 - i ** 2, -1)
+        
+        return template * 0.1
 
     def get_node_color_representation(self, position):
         color = [0, 0, 0]
@@ -164,7 +192,8 @@ class PointGrid:
     
     def get_colored_grid(self):
         color_grid = np.zeros((self.array_shape[0], self.array_shape[1], 3), dtype=np.uint8)
-
+        
+        color_grid[:, :, 1] = self.navigation_preference_grid[:, :]
         color_grid[self.traversable_grid] = (255, 0, 0)
         color_grid[self.occupied_grid] = (255, 255, 255)
 
@@ -177,5 +206,5 @@ class PointGrid:
         return color_grid
     
     def print_grid(self):
-        #cv.imshow("circle_template", self.robot_circle_template.astype(np.uint8) * 255)
-        cv.imshow("test", self.get_colored_grid())
+        cv.imshow("circle_template", self.preference_template / 4)
+        #cv.imshow("test", self.get_colored_grid())