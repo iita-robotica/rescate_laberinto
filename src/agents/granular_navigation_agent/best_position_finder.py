d74d51a7e6eba05d32c93053f7af8c38d538a45d
ale <aledeum.saf@gmail.com>
2023-04-12 14:29:11 -0300

    fixed bug with camera povs and refactored

--- src/agents/granular_navigation_agent/best_position_finder.py
+++ src/agents/granular_navigation_agent/best_position_finder.py
@@ -7,46 +7,53 @@ import math
 import skimage
 from copy import copy, deepcopy
 from algorithms.np_bool_array.bfs import BFSAlgorithm, NavigatingBFSAlgorithm
+from flags import SHOW_BEST_POSITION_FINDER_DEBUG
 
 class BestPositionFinder:
+    """
+    Finds the best position for the robot to go to, with the objective of exploring the maze.
+    """
     def __init__(self, mapper: Mapper) -> None:
         self.mapper = mapper
-        self.closest_not_seen_grid_index = None
-        self.closest_not_seen_finder = NavigatingBFSAlgorithm(found_function=lambda x: x == False, 
-                                                              traversable_function=lambda x: x == False)
+        self.closest_unseen_finder = NavigatingBFSAlgorithm(found_function=lambda x: x == False, 
+                                                            traversable_function=lambda x: x == False)
+        
+        self.closest_unseen_grid_index = None
+        
 
     def calculate_best_position(self, finished_path):
-        if self.closest_not_seen_grid_index is None:
-            self.closest_not_seen_grid_index = self.get_best_grid_index()
+        """
+        Calculate closest unseen position only when the robot has reached the previous one or if the objective
+        is no longer traversable.
+        """
+        if self.is_objective_untraversable() or finished_path:
+            self.closest_unseen_grid_index = self.get_closest_unseen_grid_index()
 
-        closest_not_seen_array_index = self.mapper.granular_grid.grid_index_to_array_index(self.closest_not_seen_grid_index)
+        # DEBUG
+        if SHOW_BEST_POSITION_FINDER_DEBUG:
+            closest_unseen_array_index = self.mapper.granular_grid.grid_index_to_array_index(self.closest_unseen_grid_index)
+            debug_grid = self.mapper.granular_grid.get_colored_grid()    
+            cv.circle(debug_grid, (closest_unseen_array_index[1], closest_unseen_array_index[0]), 4, (0, 255, 100), -1)
+            cv.imshow("closest_position_finder_debug", debug_grid)
 
+    def is_objective_untraversable(self):
+        if self.closest_unseen_grid_index is None: return False
 
-        objective_not_traversable = self.mapper.granular_grid.arrays["traversable"][closest_not_seen_array_index[0], closest_not_seen_array_index[1]]
-
-        if objective_not_traversable or finished_path:
-            self.closest_not_seen_grid_index = self.get_best_grid_index()
-
-                
-        debug_grid = self.mapper.granular_grid.get_colored_grid()    
-        cv.circle(debug_grid, (closest_not_seen_array_index[1], closest_not_seen_array_index[0]), 4, (0, 255, 100), -1)
-        cv.imshow("closest_position_finder_debug", debug_grid)
+        closest_unseen_array_index = self.mapper.granular_grid.grid_index_to_array_index(self.closest_unseen_grid_index)
+        return self.mapper.granular_grid.arrays["traversable"][closest_unseen_array_index[0], closest_unseen_array_index[1]]
     
+    def get_closest_unseen_grid_index(self):
+        robot_array_index = self.mapper.granular_grid.coordinates_to_array_index(self.mapper.robot_position)
 
-    def get_best_grid_index(self):
-        print("Calculando punto")
-        robot_grid_index = self.mapper.granular_grid.coordinates_to_grid_index(np.array(self.mapper.robot_position))
-        robot_array_index = self.mapper.granular_grid.grid_index_to_array_index(robot_grid_index)
-
-        closest_not_seen_array_index = self.closest_not_seen_finder.bfs(found_array=self.mapper.granular_grid.arrays["seen_by_camera"],
-                                                                        traversable_array=self.mapper.granular_grid.arrays["traversable"],
-                                                                        start_node=robot_array_index)
+        closest_unseen_array_index = self.closest_unseen_finder.bfs(found_array=self.mapper.granular_grid.arrays["seen_by_camera"],
+                                                                    traversable_array=self.mapper.granular_grid.arrays["traversable"],
+                                                                    start_node=robot_array_index)
         
-        return self.mapper.granular_grid.array_index_to_grid_index(closest_not_seen_array_index)
+        return self.mapper.granular_grid.array_index_to_grid_index(closest_unseen_array_index)
 
     def get_best_position(self):
-        if self.closest_not_seen_grid_index is not None:
-            coords = self.mapper.granular_grid.grid_index_to_coordinates(self.closest_not_seen_grid_index)
+        if self.closest_unseen_grid_index is not None:
+            coords = self.mapper.granular_grid.grid_index_to_coordinates(self.closest_unseen_grid_index)
             return Position2D(coords)
         else:
             return self.mapper.robot_position
