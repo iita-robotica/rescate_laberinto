252918ec1b30248d673e9fea79cf31c89a82b8e1
ale <aledeum.saf@gmail.com>
2023-04-13 12:05:46 -0300

    added docstrings

--- src/flow_control/step_counter.py
+++ src/flow_control/step_counter.py
@@ -1,4 +1,9 @@
 class StepCounter:
+    """
+    Allows to execute actions every n number of timesteps. This can be useful for performance, as it enables the program
+    to execute taxing tasks sparsely while not interrupting actions that must run constantly.
+    """
+
     def __init__(self, interval):
         self.__current_step = 0
         self.interval = interval
