5f15586afff6bac7e82cbd47d117a038254e898a
iitaadmin <aledeum.saf@gmail.com>
2023-06-15 16:42:53 -0300

    set all flags to 0

--- src/flags.py
+++ src/flags.py
@@ -1,16 +1,16 @@
 
 SHOW_FIXTURE_DEBUG = 0
-SHOW_DEBUG = False
+SHOW_DEBUG = 0
 
 SHOW_GRANULAR_NAVIGATION_GRID = 0
-SHOW_PATHFINDING_DEBUG = False
-SHOW_BEST_POSITION_FINDER_DEBUG = False
+SHOW_PATHFINDING_DEBUG = 0
+SHOW_BEST_POSITION_FINDER_DEBUG = 0
 
-SHOW_MAP_AT_END = False
+SHOW_MAP_AT_END = 0
 
 DO_WAIT_KEY = 0
 
-DO_SLOW_DOWN = False
+DO_SLOW_DOWN = 0
 SLOW_DOWN_S = 0.032
 
 
@@ -20,5 +20,5 @@ SAVE_FIXTURE_DEBUG_DIR = "/home/iitaadmin/simulated_rescue_maze/debug_imgs"
 DO_SAVE_FINAL_MAP = 0
 SAVE_FINAL_MAP_DIR = "/home/iitaadmin/simulated_rescue_maze/final_maps"
 
-DO_SAVE_DEBUG_GRID = True
+DO_SAVE_DEBUG_GRID = 0
 SAVE_DEBUG_GRID_DIR = "/home/iitaadmin/simulated_rescue_maze/final_maps"
